ca76ea109f893dfd915af2b78237b050b4dc38b4
James Furey <jfurey@gmail.com>
2017-12-07 19:43:47 +1100

    Patched PHP short tags render bug in "includes" file.

--- resources/views/includes.blade.php
+++ resources/views/includes.blade.php
@@ -1,4 +1,4 @@
-<?placeholder
+<php
 
 use Ajthinking\Tinx\Console\State;
 
--- src/Includes/IncludeManager.php
+++ src/Includes/IncludeManager.php
@@ -14,7 +14,7 @@ class IncludeManager
 
         $contents = view('tinx::includes', compact('names', 'config'))->render();
 
-        $contents = str_replace_first('<?placeholder', '<?php', $contents);
+        $contents = str_replace_first('<php', '<?php', $contents);
 
         app('tinx.storage')->put('includes.php', $contents);
     }
